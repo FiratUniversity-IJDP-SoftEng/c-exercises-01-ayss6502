779ad0b45c3133b70800dbcc6f595e4614629b09
ayss6502 <aa.seednz2323@gmail.com>
2025-10-28 11:29:50 +0300

    Update ex5.c

--- ex5.c
+++ ex5.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+void sayHello(char name[]){
+  printf("Hello, %s\n", name);
+}
+int main() {
+  char name[100];
+  printf("What's your name? ");
+  scanf("%99s", name);
+  sayHello(name);
+}
