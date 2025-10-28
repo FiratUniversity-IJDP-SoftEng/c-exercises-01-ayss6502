09a61c5d059abfa006eaa80132a7572cb820788c
ayss6502 <aa.seednz2323@gmail.com>
2025-10-28 11:11:05 +0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+  int age;
+  printf("How old are you? ");
+  scanf("%d",&age);
+
+  for ( int i=1; int <= age; i++) {
+     printf("Happy Birthdayy!\n");
+  }
+}
+
+    
