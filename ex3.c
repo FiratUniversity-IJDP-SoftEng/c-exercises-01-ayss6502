260c9429eb6686dcf9dee74923312bb9df36a9dd
ayss6502 <aa.seednz2323@gmail.com>
2025-10-28 11:22:04 +0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -1,2 +1,17 @@
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+
+  char pet[20];
+  printf("Do you have a cat or dog? ");
+  scanf("%19s", pet);
+
+if (strcmp(pet, "cat") == 0){
+  printf("meow\n");
+}
+else{
+printf("havv\n")
+}
+}
 
