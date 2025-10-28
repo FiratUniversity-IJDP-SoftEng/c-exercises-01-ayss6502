0e003ccb86966333147c998c424ae4a784f2cfc5
ayss6502 <aa.seednz2323@gmail.com>
2025-10-28 11:06:57 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+  int guess;
+  printf("Guess a number between 1 and 5");
+  scanf("%d", &guess);
+if (guess == 3) {
+  printf("true");
+}
+else {
+   printf("false");
+}
+}
+    
